0319c26867105ae6ce08f4b8cbd1275214669c78
LutchenkoOleh <160421286+LutchenkoOleh@users.noreply.github.com>
2024-06-01 17:33:09 +0200

    hw-js-6

--- js/task-2.js
+++ js/task-2.js
@@ -1,21 +1,31 @@
-const parent = {
-  name: "Stacey",
-  surname: "Moore",
-  age: 54,
-  heritage: "Irish",
-};
+class Storage {
+  #items;
+  constructor(param) {
+    this.#items = param || [];
+  }
 
-const child = Object.create(parent);
-child.name = "Jason";
-child.age = 27;
-console.log(parent.hasOwnProperty("surname"));
-console.log(parent.hasOwnProperty("heritage"));
-console.log(child.hasOwnProperty("name"));
-console.log(child.hasOwnProperty("age"));
-console.log(child.hasOwnProperty("surname"));
-console.log(parent.isPrototypeOf(child));
-console.log(child.hasOwnProperty("heritage"));
-console.log(child.surname);
-console.log(child.heritage);
-console.log(child.age);
-console.log(child.name);
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem)
+  }
+  removeItem(itemToRemove) {
+    const index = this.#items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this.#items.splice(index, 1)
+    }
+  }
+
+}
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
